7865339607b551b516890c06ddb28f9884151c72
Ryan Le <31494954+lryanle@users.noreply.github.com>
2022-11-29 02:14:24 -0600

    first half of settings

--- app/src/main/java/com/example/scannr/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/scannr/fragments/SettingsFragment.java
@@ -1,13 +1,18 @@
 package com.example.scannr.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.scannr.R;
+import com.example.scannr.family.ChildAccountManager;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -18,5 +23,13 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button addChildButton = requireView().findViewById(R.id.addChildButton);
+        addChildButton.setOnClickListener(v -> startActivity(new Intent(getActivity(), ChildAccountManager.class)));
     }
 }
